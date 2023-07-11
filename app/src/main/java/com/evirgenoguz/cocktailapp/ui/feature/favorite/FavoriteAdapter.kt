52f12bc41cf303149592cce823ec056aa07ffc71
oguzevirgen <evirgenoguz@gmail.com>
2023-07-11 15:32:18 +0300

    add | FavoriteCocktails Page

---
+++ app/src/main/java/com/evirgenoguz/cocktailapp/ui/feature/favorite/FavoriteAdapter.kt
@@ -0,0 +1,9 @@
+package com.evirgenoguz.cocktailapp.ui.feature.favorite
+
+/**
+ * @Author: Oguz Evirgen
+ * @Date: 11.07.2023
+ */
+
+class FavoriteAdapter {
+}
--- app/src/main/java/com/evirgenoguz/cocktailapp/ui/feature/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/evirgenoguz/cocktailapp/ui/feature/favorite/FavoriteViewModel.kt
@@ -1,10 +1,13 @@
 package com.evirgenoguz.cocktailapp.ui.feature.favorite
 
 import com.evirgenoguz.cocktailapp.core.BaseViewModel
+import com.evirgenoguz.cocktailapp.data.repository.CocktailRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class FavoriteViewModel @Inject constructor(): BaseViewModel() {
+class FavoriteViewModel @Inject constructor(
+    private val cocktailRepository: CocktailRepository
+): BaseViewModel() {
     // TODO: Implement the ViewModel
 }
